ee379d05af5c6bf894a91a15290b27668a890e69
Mohamed Ramadan <60930725+MoRmdn@users.noreply.github.com>
2022-09-16 10:24:22 +0200

    add helper class

--- lib/authentication/model/user.dart
+++ lib/authentication/model/user.dart
@@ -1,18 +1,19 @@
 class Users {
-  Users(
-      {required this.id,
-      required this.name,
-      required this.email,
-      required this.phone,
-      this.descreption,
-      this.titel});
+  Users({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.phone,
+    this.description,
+    this.title,
+  });
 
   String? id;
   String? name;
   String? email;
-  String? titel;
+  String? title;
   String? phone;
-  String? descreption;
+  String? description;
 
   factory Users.fromJson(Map json) => Users(
         id: json["id"],
@@ -26,7 +27,52 @@ class Users {
         "name": name,
         "email": email,
         "phone": phone,
-        "descreption": descreption,
-        "titel": titel
+        "description": description,
+        "title": title
+      };
+}
+
+class UserHelper {
+  UserHelper({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.phone,
+    this.description,
+    this.title,
+  });
+
+  String? id;
+  String? name;
+  String? email;
+  String? title;
+  String? phone;
+  String? description;
+
+  factory UserHelper.fromJson(Map? json) {
+    if (json != null) {
+      return UserHelper(
+        id: json["id"] ?? '',
+        name: json["name"] ?? '',
+        email: json["email"] ?? '',
+        phone: json["phone"] ?? '',
+      );
+    } else {
+      return UserHelper(
+        id: '',
+        name: '',
+        email: '',
+        phone: '',
+      );
+    }
+  }
+
+  Map<String, Object?> toJson() => {
+        "id": id,
+        "name": name,
+        "email": email,
+        "phone": phone,
+        "description": description,
+        "title": title
       };
 }
